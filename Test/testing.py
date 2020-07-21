233e597628e4f2c5ecac5b99b7331d3fb2a92b6e
ninadmohale <nrmohale@mtu.edu>
2020-07-21 18:23:06 -0400

    Added more test files

--- Test/testing.py
+++ Test/testing.py
@@ -5,9 +5,16 @@ Created on Mon Jul 13 19:20:15 2020
 @author: 12699
 """
 
-from pyfixorchestra import FixDictionary
+# from pyfixorchestra import FixDictionary
+#
+# test = FixDictionary("components")
+# asd = (test.generateDictionary())
+#
+# print((asd.keys()))
 
-test = FixDictionary("components")
-asd = (test.generateDictionary())
 
-print((asd.keys()))
+from tagvalueprettyprinter import PrettyPrinter
+
+
+pp = PrettyPrinter()
+tagvalue_line_eg = pp.prettify('fix_example_jim.txt')
